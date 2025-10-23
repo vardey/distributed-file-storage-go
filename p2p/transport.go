d38c8adca567ab1dde1648a250d0ea7f2f6327f2
Kunal Vardey <vardeyk@gmail.com>
2025-10-23 21:55:58 +0530

    add: Transport and Peer transport

---
+++ p2p/transport.go
@@ -0,0 +1,7 @@
+package p2p
+
+// Transport represents anything that handles network communication.
+type Transport interface{}
+
+// Peer represents a remote node in the network.
+type Peer interface{}
