a6b88ed72a315281c05998b24e4f13683bee900a
Kunal Vardey <vardeyk@gmail.com>
2025-10-24 12:35:09 +0530

    update: Transport interface

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,7 +1,9 @@
 package p2p
 
 // Transport represents anything that handles network communication.
-type Transport interface{}
+type Transport interface {
+	ListenAndAccept() error
+}
 
 // Peer represents a remote node in the network.
 type Peer interface{}
