7ad0a78d02e3e102ab021f333d72221b772403a4
Kunal Vardey <vardeyk@gmail.com>
2025-10-24 12:34:12 +0530

    add: ListenAndAccept,  startAcceptLoop  and handleConnection

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -16,4 +17,27 @@ type TCPTransport struct {
 func NewTCPTransport(listenAddress string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddress}
+}
+
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+	return nil
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	conn, err := t.listener.Accept()
+	if err != nil {
+		fmt.Printf("TCP start and accept error: %s\n", err)
+	}
+
+	go t.handleConnection(conn)
+}
+
+func (t *TCPTransport) handleConnection(conn net.Conn) {
+	fmt.Printf("New Connection incoming: %+v\n", conn)
 }
