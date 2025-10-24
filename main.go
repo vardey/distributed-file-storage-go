74e9ed36aa3d7f6f81c362e16746968b7cf565db
Kunal Vardey <vardeyk@gmail.com>
2025-10-24 19:09:28 +0530

    add: code to test

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/kunalvardey/distributed-file-storage-go/p2p"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	tr := p2p.NewTCPTransport(":8080")
+
+	if err := tr.ListenAndAccept(); err != nil {
+		log.Fatal(err)
+	}
+	select {}
 }
