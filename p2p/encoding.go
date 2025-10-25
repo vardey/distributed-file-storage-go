2b8ff9368406af285e339c8c004073d08824ee1e
Kunal Vardey <vardeyk@gmail.com>
2025-10-25 10:07:49 +0530

    add:  GOBDecoder and Decode

--- p2p/encoding.go
+++ p2p/encoding.go
@@ -1,5 +1,16 @@
 package p2p
 
+import (
+	"encoding/gob"
+	"io"
+)
+
 type Decoder interface {
-	Decode(any) error
+	Decode(io.Reader, any) error
+}
+
+type GOBDecoder struct{}
+
+func (dec GOBDecoder) Decode(r io.Reader, v any) error {
+	return gob.NewDecoder(r).Decode(v)
 }
