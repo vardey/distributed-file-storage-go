4f7298a01af067ae4c8333467c1e56399ae6c42e
Kunal Vardey <vardeyk@gmail.com>
2025-10-25 23:24:46 +0530

    add: Consume to Transport and Close to Peer

--- p2p/transport.go
+++ p2p/transport.go
@@ -3,7 +3,10 @@ package p2p
 // Transport represents anything that handles network communication.
 type Transport interface {
 	ListenAndAccept() error
+	Consume() <-chan RPC
 }
 
 // Peer represents a remote node in the network.
-type Peer interface{}
+type Peer interface {
+	Close() error
+}
